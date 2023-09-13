8ff8bbb4330bb7a919281fa78268b26ad823a491
hamzaoui_hamza <hamzaoui.h@outlook.com>
2023-09-13 09:40:15 +0200

    ajout doss spring

---
+++ backEnd/ProxiBanque_HH_ZA/src/main/java/com/example/ProxiBanque_HH_ZA/ProxiBanqueHhZaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProxiBanque_HH_ZA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueHhZaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxiBanqueHhZaApplication.class, args);
+	}
+
+}
---
+++ backEnd/ProxiBanque_HH_ZA/src/test/java/com/example/ProxiBanque_HH_ZA/ProxiBanqueHhZaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProxiBanque_HH_ZA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiBanqueHhZaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
