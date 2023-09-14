970abc5f8bcc25cdf25787dd06af4bb102cc7967
hamzaoui_hamza <hamzaoui.h@outlook.com>
2023-09-14 19:23:57 +0200

    crud clien conseiller compte courant/epargne

---
+++ backEnd/ProxiBanque_H_Z/src/main/java/com/example/ProxiBanque_HH_ZA/ProxiBanque_HH_ZA.java
@@ -0,0 +1,16 @@
+
+package com.example.ProxiBanque_HH_ZA;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanque_HH_ZA {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanque_HH_ZA.class, args);
+    }
+
+}
+
