a69ce4cb2777bed0f144581c26dc480863cbdeed
hamzaoui_hamza <hamzaoui.h@outlook.com>
2023-09-14 11:49:10 +0200

    sec

--- backEnd/ProxiBanque_H_Z/src/main/java/com/example/ProxiBanque_HH_ZA/lunch.java
+++ backEnd/ProxiBanque_H_Z/src/main/java/com/example/ProxiBanque_HH_ZA/lunch.java
@@ -1,13 +1,13 @@
 package com.example.ProxiBanque_HH_ZA;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProxiBanqueHhZaApplication {
+public class lunch {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxiBanqueHhZaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(lunch.class, args);
+    }
 
 }
+
